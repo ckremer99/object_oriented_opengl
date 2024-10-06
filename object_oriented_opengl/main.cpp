cd349dd107ac8356f2855b2d4ab2a12cbac44fe0
ckremer99 <ckremer99@outlook.com>
2024-10-06 15:52:53 -0700

    Initial Commit

---
+++ object_oriented_opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  object_oriented_opengl
+//
+//  Created by Cody Kremer on 10/6/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
